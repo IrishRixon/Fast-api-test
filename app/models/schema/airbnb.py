a4a0251c0102ee427e35605732561fde5d572fbc
IrishRixon <irishrixon@gmail.com>
2026-06-26 13:17:49 +0800

    feat: model

---
+++ app/models/schema/airbnb.py
@@ -0,0 +1,39 @@
+from motor.core import AgnosticClientSession
+from typing import Optional
+from typing import Dict
+from typing import List
+from typing import Tuple
+from typing import Any
+from bson import ObjectId
+from motor.motor_asyncio import AsyncIOMotorDatabase
+class AirBnbModel(): 
+    collection_name = "listingAndReviews"
+
+    def __init__(self, db: AsyncIOMotorDatabase): 
+        self.collection = db[self.collection_name]
+
+    def _convert_objectids_to_str(self, doc: Any) -> Any:
+        if isinstance(doc, list):
+            return [self._convert_objectids_to_str(i) for i in doc]
+        if isinstance(doc, dict):
+            return {k: self._convert_objectids_to_str(v) for k, v in doc.items()}
+        if isinstance(doc, ObjectId):
+            return str(doc)
+        return doc        
+
+    async def get_airbnb_paginated(self, limit: int = 10, skip: int = 0, session: Optional[AgnosticClientSession] = None) -> Tuple[List[Dict[str, Any]], int]: 
+        total_count = await self.collection.count_documents({}, session=session)
+        
+        docs = await self.collection.aggregate([
+            {"$skip": skip},
+            {"$limit": limit}
+        ], session=session).to_list(limit)
+
+        result = [self._convert_objectids_to_str(doc) for doc in docs]
+
+        return (result, total_count)
+
+
+
+
+        
